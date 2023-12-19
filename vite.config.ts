c6c2381b73800de43a7117b3466109da66cd13ee
Alexandr Alexandrov <alxcube@gmail.com>
2023-12-19 21:52:38 +0200

    configured base path

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/lens-playground/',
   plugins: [
     vue({
       template: { transformAssetUrls }
